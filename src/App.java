4fb72f98cd4aca066bdbfd1f96033785ff909532
taku1029-dev <taku1029.dev@gmail.com>
2024-09-23 02:37:19 +0800

    Test login system

--- src/App.java
+++ src/App.java
@@ -1,23 +1,11 @@
-import java.awt.FlowLayout;  
-import javax.swing.JButton;  
-import javax.swing.JFrame;  
-import javax.swing.JLabel;  
-import javax.swing.JPanel;  
+import LoginSystem.IDandPasswords;
+import LoginSystem.LoginPage;  
 
 public class App {
     public static void main(String[] args) throws Exception {
-        JFrame frame = new JFrame("JFrame Example");  
-        JPanel panel = new JPanel();  
-        panel.setLayout(new FlowLayout());  
-        JLabel label = new JLabel("JFrame By Example");  
-        JButton button = new JButton();  
-        button.setText("Button");  
-        panel.add(label);  
-        panel.add(button);  
-        frame.add(panel);  
-        frame.setSize(200, 300);  
-        frame.setLocationRelativeTo(null);  
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  
-        frame.setVisible(true);  
+        IDandPasswords idandPasswords = new IDandPasswords();
+
+        LoginPage loginPage = new LoginPage(idandPasswords.getLogininfo());
+
     }
 }
--- src/LoginSystem/IDandPasswords.java
+++ src/LoginSystem/IDandPasswords.java
@@ -1,5 +1,20 @@
 package LoginSystem;
 
+import java.util.HashMap;
+
 public class IDandPasswords {
 
+    HashMap<String, String> logininfo = new HashMap<String, String>();
+
+    public IDandPasswords() {
+
+        logininfo.put("username", "pass");
+        logininfo.put("username1", "pass1");
+        logininfo.put("username2", "pass2");
+        logininfo.put("username3", "pass3");
+    }
+
+    public HashMap getLogininfo() {
+        return logininfo;
+    }
 }
