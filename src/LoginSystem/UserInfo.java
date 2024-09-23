682b01dccea84d2755038fee1bc933f5f0f63526
taku1029-dev <taku1029.dev@gmail.com>
2024-09-23 14:43:01 +0800

    Add UserInfo class

---
+++ src/LoginSystem/UserInfo.java
@@ -0,0 +1,29 @@
+package LoginSystem;
+
+import java.util.HashMap;
+
+// public class UserInfo {
+
+//     HashMap<String, String> logininfo = new HashMap<String, String>();
+
+//     public UserInfo() {
+
+//         logininfo.put("username", "pass");
+//         logininfo.put("username1", "pass1");
+//         logininfo.put("username2", "pass2");
+//         logininfo.put("username3", "pass3");
+//     }
+
+//     public HashMap getLogininfo() {
+//         return logininfo;
+//     }
+// }
+
+public abstract class UserInfo {
+    private int[][] idandPass;
+
+    // public abstract int[][] getLoginInfo();
+    public void test() {
+        System.out.println("UserInfo Class");
+    }
+}
